99c1eb8c710c4d81bdc331b34e59f9a443251361
lidiyag <lidiya.nixon@gmail.com>
2023-10-16 22:29:21 -0700

    Updates to just hello world

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -22,45 +19,6 @@ var dbErr error
 
 func main() {
 
-	// router setup
-	router := gin.Default()
-
-	router.Any("/healthz", healthCheck)
-
-	router.Run()
-
-}
-
-func healthCheck(c *gin.Context) {
-
-	// Set Cache Control
-	c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
-	c.Header("Pragma", "no-cache")
-	c.Header("X-Content-Type-Options", "nosniff")
-
-	// Check for http method
-	if c.Request.Method != http.MethodGet {
-		c.Status((http.StatusMethodNotAllowed))
-		return
-	}
-
-	// Payload Check
-	if c.Request.ContentLength > 0 {
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	// Connection String
-	dbConn := dbUser + ":" + dbPassword + "@tcp" + "(" + dbHost + ":" + dbPort + ")/" + dbName + "?" + "parseTime=true&loc=Local"
-	fmt.Println("DB Connection String ", dbConn)
-	_, dbConnErr := gorm.Open(mysql.Open(dbConn), &gorm.Config{})
-	fmt.Printf("DB Connection Status: error=%v", dbConnErr)
-
-	// DB Connection Check
-	if dbConnErr != nil {
-		c.Status(http.StatusServiceUnavailable)
-	} else {
-		c.Status(http.StatusOK)
-	}
+	fmt.Print("HELLO WORLD")
 
 }
