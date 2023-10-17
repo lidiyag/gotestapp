1e841e046f3398dc90ff4b977b523e49612c3438
lidiyag <lidiya.nixon@gmail.com>
2023-10-16 22:30:36 -0700

    new update

--- main.go
+++ main.go
@@ -2,20 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"gorm.io/gorm"
 )
-
-var dbUser = os.Getenv("DB_USER")
-var dbPassword = os.Getenv("DB_PASSWORD")
-var dbName = os.Getenv("DB_NAME")
-var dbHost = os.Getenv("DB_HOST")
-var dbPort = os.Getenv("DB_PORT")
-var usersFilePath = os.Getenv("USERS_PATH")
-
-var db *gorm.DB
-var dbErr error
 
 func main() {
 
